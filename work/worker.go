314359714c3655aad28bbe554b9781e91bfa888b
Harsha Vardhan Gelivi <harsha.vardhan@gl0091-x0.corp.inmobi.com>
2020-08-07 00:30:17 +0530

    fix for

--- work/worker.go
+++ work/worker.go
@@ -25,25 +25,30 @@ func (w *Worker) sessionRun() {
 	minuteStart := time.Now()
 	numRecs := 0
 	totalRecs := 0
-	select {
-	case datum := <-w.DataChannel:
-		sessionData[sessionIdx] = datum
-		sessionIdx++
-		numRecs++
-		if sessionIdx == w.BuffSize {
-			e := w.DstSink(sessionData)
-			sessionIdx = 0
-			if e != nil {
-				// todo add more handling/exponential retry
-				fmt.Println(e)
+	loop:
+	for {
+		select {
+		case datum, ok := <-w.DataChannel:
+			if !ok {
+				break loop
 			}
-		}
+			sessionData[sessionIdx] = datum
+			sessionIdx++
+			numRecs++
+			if sessionIdx == w.BuffSize {
+				e := w.DstSink(sessionData)
+				sessionIdx = 0
+				if e != nil {
+					// todo add more handling/exponential retry
+					fmt.Println(e)
+				}
+			}
 		case <-time.Tick(time.Second * 60):
 			fmt.Printf("Handled %d in %v slot\n", numRecs, time.Since(minuteStart))
 			minuteStart = time.Now()
-			numRecs = 0
 			totalRecs += numRecs
-
+			numRecs = 0
+		}
 	}
 	fmt.Printf("Handled %d in %v\n", totalRecs, time.Since(start))
 }
